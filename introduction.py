53bb5869700d610b663ec8a991a4ebf82c9cbba9
ramaphosa008 <cyrilmutua719@gmail.com>
2026-02-03 10:26:07 +0300

    Python

---
+++ introduction.py
@@ -0,0 +1,11 @@
+# Text are written within quotation marks
+# print is a method used to display content(an output)
+print("Welcome to Python Programming")
+print("Cyril")
+
+# No.s are entered within the brackets without quotations
+print(100)
+print(20*3)
+
+
+
---
+++ variables.py
@@ -0,0 +1,22 @@
+#A VARIABLE is a container used to store a value
+
+school = "eMobilis"
+firstname = "Cyril"
+lastname = "Ramaphosa"
+num1 = 36
+num2 = 42
+language = "Python"
+
+print(school)
+print(firstname, lastname)
+print(num1 + num2)
+print(language)
+print(num2 - num1)
+print(num1 * num2)
+
+print(language, "is a web programming language")
+
+print(school, "is an institution that trains students on web and mobile app development.")
+
+
+print("The sum of", num1, "and", num2, "is", num1+num2)
