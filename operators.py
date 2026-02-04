472c0a8beda7f2fd3998b5426cfb50d285dbd04d
ramaphosa008 <cyrilmutua719@gmail.com>
2026-02-04 10:22:17 +0300

    New1

---
+++ operators.py
@@ -0,0 +1,28 @@
+# ARITHMETIC OPERATORS
+x = 20
+y = 6
+
+print(x + y)
+
+print(x - y)
+
+print(x / y)
+
+print(x * y)
+
+print(x % y) #Modulus - returns remainder after
+
+
+#COMPARISON OPERATORS - all about comparing
+print(x < y)
+
+print(x > y)
+
+print(x <= y)
+
+print(x >= y)
+
+print(x == y)
+
+print(x != y)  # ! - represents 'NOT' in the defined comparison
+
