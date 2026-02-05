66d363ab64a84bc4170ecb1fb08d3b21e501e6a9
ramaphosa008 <cyrilmutua719@gmail.com>
2026-02-05 09:57:11 +0300

    New2

---
+++ controlstatements.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter current room temperature : "))
+
+if temperature > 25:
+    print("It is too hot")
+else:
+    print("It is too cold")
---
+++ largestnumber.py
@@ -0,0 +1,7 @@
+
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+third = int(input("Enter the third number: "))
+
+if first > second and first > third:
+    print(first, " is the gratest number")
