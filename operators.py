e79f35129bc5c8dc0880b22465286781a4c6d3b6
ramaphosa008 <cyrilmutua719@gmail.com>
2026-02-05 09:36:17 +0300

    New2

--- operators.py
+++ operators.py
@@ -22,7 +22,42 @@ print(x <= y)
 
 print(x >= y)
 
-print(x == y)
+print(x == y) #Equal to
 
 print(x != y)  # ! - represents 'NOT' in the defined comparison
 
+
+#ASSIGNMENT OPERATORS - used to assign/store values in variables
+# (=)
+number = 45
+print(number)
+
+#(+)
+number += 2
+print(number)
+
+#(-)
+number -= 2
+print(number)
+
+#(*)
+number *= 2
+print(number)
+
+#(**)
+number **= 2
+print(number)
+
+#LOGICAL OPERATORS
+num1 = 67
+num2 = 43
+
+#AND - returns true if all/both conditions are true, returns false if one condition is a false.
+print(num1 == num2 and num1 > num2)
+
+#OR - returns True if one of the statements is true, returns false if both conditions are false
+print(num1 == num2 or num1 > num2)
+
+#NOT - Reverse the result, returns False if the result is true
+print(not (num1 == num2 or num1 > num2))
+print(not (num1 == num2 and num1 > num2))
---
+++ userinputoutput.py
@@ -0,0 +1,23 @@
+
+firstname = input("Please enter your firstname: ")
+print("My firstname is", firstname)
+
+print()
+
+color = input("Enter your favourite color: ")
+print("My favourite color is", color)
+
+print()
+
+age = int(input("How old are you?(yrs) : "))
+print("i am", age, "years old")
+
+print()
+
+height = float(input("Enter your height: "))
+print ("I am", height, "cm tall")
+
+print()
+print()
+
+print("Thank you for your time")
