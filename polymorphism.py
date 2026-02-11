cee9777b5327d7fa6b98b4cd0de11f13c4e0d4f7
ramaphosa008 <cyrilmutua719@gmail.com>
2026-02-11 10:19:06 +0300

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,21 @@
+class rectangle:
+    def draw(self):
+        print("Drawing a rectangle")
+
+class rhombus:
+    def draw(self):
+        print("Drawing a rhombus")
+
+class parallelogram:
+    def draw(self):
+        print("Drawing a parallelogram")
+
+
+rect1 = rectangle()
+rect1.draw()
+
+rho1 = rhombus()
+rho1.draw()
+
+par1 = parallelogram()
+par1.draw()
